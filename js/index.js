6e37ac94e9044b9ac2e028bcc45a0419477d5591
muhamadrestu <mrestuali44@gmail.com>
2022-06-05 23:16:54 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,14 +1,12 @@
 var mySong = document.getElementById("mySong");
 var icon = document.getElementById("myNav");
 
-var icon2 = document.getElementById("myNav");
-
 icon.onclick = function () {
   if (mySong.paused) {
     mySong.play();
-    icon.src = "/images/pause2.png";
+    icon.src = "/images/play2.png";
   } else {
     mySong.pause();
-    icon.src = "/images/play2.png";
+    icon.src = "/images/pause2.png";
   }
 };
